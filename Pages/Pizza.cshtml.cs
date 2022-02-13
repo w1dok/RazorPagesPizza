ab485fa9efc4c27b86db099751de0aba540219a3
atokarew <free_m1nd@mail.ru>
2022-02-13 18:27:07 +0300

    end

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
 
 namespace RazorPagesPizza.Pages
 {
@@ -7,6 +9,35 @@ namespace RazorPagesPizza.Pages
     {
         public void OnGet()
         {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public List<Pizza> pizzas = new();
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if (pizza.IsGlutenFree)
+                return "Gluten Free";
+            return "Not Gluten Free";
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = new();
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
         }
     }
 }
